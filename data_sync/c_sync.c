cb1030070fe8ea3826cf9c0f5694b1ff6af5e648
zz chen <grantchen88@gmail.com>
2013-04-09 08:38:23 +0800

    some modification,it seems work now

--- data_sync/c_sync.c
+++ data_sync/c_sync.c
@@ -1,25 +1,79 @@
 #include"glob.h"
 #include"rsync.h"
 #define CBUFSZ	(BLK_SZ+DELTA_BLOCK_ENTRY_SZ+1)
+static void * do_delta_blk(void * arg)
+{
+	u64 d_off,blk_nr = ((thread_arg*)arg)->blk_nr;
+	u32 d_old_len,d_new_len;
+	u8 d_embeded;
+	u8 buf[CBUFSZ];
+	int blk_counter = 0;
+	int connfd = ((thread_arg *)arg)->connfd;
+	int fd = ((thread_arg *)arg)->fd;
+	int n;
+	u64 i;
+	delta_block_entry dblk;
+	for(i = 0;i<blk_nr;i++){
+		if((n = Read(connfd,&dblk,DELTA_BLOCK_ENTRY_SZ)) != DELTA_BLOCK_ENTRY_SZ){
+			perror("Read delta_block_entry");
+			exit(1);
+		}
+		printf("---------------------Reading delta block	#%d\n",i);
+		d_embeded = dblk.embeded;
+		d_off = dblk.offset;
+		d_old_len = dblk.old_len;
+		d_new_len = dblk.new_len;
+		blk_counter = d_off/BLK_SZ;
+		if(d_off%BLK_SZ != 0){
+			fprintf(stderr,"delta_blk offset isnot aligned to BLK_SZ!\n");
+			goto thRead_exit;
+		}
+		printf("delta_blk_old_len #%d\n",d_old_len);
+		printf("delta_blk_new_len #%d\n",d_new_len);
+		printf("delta_blk_offset  #%d\n",d_off);
+		if(d_embeded == BLK_SAME){
+			/* no need to sync ,just continue */
+			printf("delta_blk_emb #BLK_SAME\n");
+			continue;
+		}
+		printf("delta_blk_emb #BLK_DIFF\n");
+		/* need overWrite */
+		bzero(buf,CBUFSZ);
+		if((n = Read(connfd,buf,d_new_len)) != d_new_len){
+			/* something is wrong */
+			perror("Read from socket");
+			goto thRead_exit;
+		}
+		*(buf + n) = '\0';
+		printf("delta content \n%s\n",buf);
+		lseek(fd,d_off,SEEK_SET);
+		if((n = Write(fd,buf,d_new_len)) != d_new_len){
+			/* something is wrong */
+			perror("Write to file");
+			goto thRead_exit;
+		}
+		printf("--------------------------------------delta blk #%d ok!\n",i);
+	}
+thRead_exit:
+	pthread_exit(NULL);
+}
 int main(int argc,char * argv[])
 {
+	pthread_t thid;
+	thread_arg arg;
 	int fd,n;
-	pid_t pid;
 	u8 buf[CBUFSZ];
 	struct stat cstat;
 	chunk_file_header cfh;
+	server_file_header sfh;
 	chunk_block_entry cblk;
-	delta_block_entry dblk;
-	u64 d_off;
-	u32 d_old_len,d_new_len;
-	u8 d_embeded;
 	u32 block_sz;
 	u64 block_nr;
 	u64 c_off,file_sz,i,remaining_bytes;
+	u64 srv_file_sz;
 	u32 c_len;
 	u8 * file_name;
     int connfd,clen;
-    int res;
     struct sockaddr_in addr;
 	if(argc != 2){
 		fprintf(stderr,"invalid argument!\n");
@@ -39,9 +93,13 @@ int main(int argc,char * argv[])
 	if(file_sz%BLK_SZ != 0){
 		block_nr += 1;
 	}
-	cfh.block_nr = block_nr;
+	cfh.file_sz = file_sz;
 	cfh.block_sz = block_sz;
 	strncpy(cfh.fn,file_name,FILE_NAME_LEN);
+	printf("file_to_sync	# %s\n",cfh.fn);
+	printf("file_sz			# %d\n",cfh.file_sz);
+	printf("block_sz		# %d\n",cfh.block_sz);
+	printf("block_nr		# %d\n",block_nr);
     bzero(&addr,sizeof(addr));
     addr.sin_family = AF_INET;
     inet_pton(AF_INET,RSYNC_HOST_IP,(void *)&addr.sin_addr);
@@ -55,64 +113,96 @@ int main(int argc,char * argv[])
         perror("Connection");
 		goto over1;
     }
-	if((pid = fork()) == 0){
-		/* child process receive the delta blk */
-		while((n = read(connfd,&dblk,DELTA_BLOCK_ENTRY_SZ)) == DELTA_BLOCK_ENTRY_SZ){
-			d_embeded = dblk.embeded;
-			d_off = dblk.offset;
-			d_old_len = dblk.old_len;
-			d_new_len = dblk.new_len;
-			if(d_embeded == BLK_SAME){
-				/* no need to sync ,just continue */
-				continue;
-			}
-			if(d_old_len > d_new_len){
-				/* need truncate */
-				ftruncate(fd,d_off + d_new_len);
-				if(d_new_len == 0){
-					break;
-				}
-			}
-			/* need overwrite */
-			bzero(buf,CBUFSZ);
-			if((n = read(connfd,buf,d_new_len)) != d_new_len){
-				/* something is wrong */
-				perror("read from socket");
-				exit(1);
-			}
-			lseek(fd,d_off,SEEK_SET);
-			if((n = write(fd,buf,d_new_len)) != d_new_len){
-				/* something is wrong */
-				perror("write to file");
-				exit(1);
-			}
-		}
-		close(connfd);
-		close(fd);
-		exit(0);
+	/*---------------- Write chunk_file_header to server ------------------*/
+	if((n = Write(connfd,&cfh,CHUNK_FILE_HEAD_SZ)) != CHUNK_FILE_HEAD_SZ){
+		perror("Write chunk_file_header");
+		goto over1;
 	}
-	if((n = write(connfd,&cfh,CHUNK_FILE_HEAD_SZ)) != CHUNK_FILE_HEAD_SZ){
-		perror("writesocket");
+	printf("------------------------ chunk file header send ok!-----------------\n");
+	if((n = Read(connfd,&sfh,SERVER_FILE_HEADER_SZ)) != SERVER_FILE_HEADER_SZ){
+		perror("Read server_file_header");
+		goto over1;
+	}
+	printf("------------------------ server file header receive ok!-------------\n");
+	if(sfh.err != E_OK){
+		printf("server file header says some error happens!\n");
+		goto over1;
+	}
+	srv_file_sz = sfh.file_sz;
+	printf("server file size		# %d\n",srv_file_sz);
+	if(srv_file_sz < file_sz){
+		printf("client file too big,need trunc\n");
+		if(ftruncate(fd,srv_file_sz) != 0){
+			perror("ftruncate");
+			goto over1;
+		}
+		fstat(fd,&cstat);
+		file_sz = cstat.st_size;
+		printf("after truncate file sz --- %d\n",file_sz);
+	}else{
+		printf("client file is smller than server file!,no need to truncate!\n");
+	}
+	block_nr = srv_file_sz/BLK_SZ;
+	if(srv_file_sz%BLK_SZ != 0){
+		block_nr += 1;
+	}
+	printf("block number in server # %d\n",block_nr);
+	printf("client send chunk block according this block_number,reguardless of the real blk_number it has\n");
+	/*********************************************************************/
+	printf("now create a new thRead to receive delta_block!\n");
+	arg.connfd = connfd;
+	arg.fd = fd;
+	arg.blk_nr = block_nr;
+	if(pthread_create(&thid,NULL,do_delta_blk,(void*)&arg) != 0){
+		perror("pthread_create");
 		goto over1;
 	}
 	for(i = 0;i < block_nr;i++){
+		printf("-------------------chunk_blk_entry		#%d\n",i);
+		bzero(&cblk,CHUNK_BLOCK_ENTRY_SZ);
 		c_off = i*block_sz;
-		remaining_bytes = file_sz - c_off;
-		c_len = (remaining_bytes<block_sz?remaining_bytes:block_sz);
 		cblk.offset = c_off;
+		printf("block offset		#%d\n",c_off);
+		if(file_sz <= c_off){
+			printf("while client file sz #%d\n",file_sz);
+			printf("out of boundry,send chunk_blk_entry with len = 0\n");
+			c_len = 0;
+			cblk.len = 0;
+			goto send_chunk_blk_entry;
+		}
+		remaining_bytes = file_sz - c_off;
+		printf("client remaining_bytes -- %d\n",remaining_bytes);
+		if(remaining_bytes >= block_sz){
+			printf("remaining_bytes is not less than block_sz\n");
+			c_len = block_sz;
+		}else{
+			printf("remaining_bytes is less than block_sz\n");
+			c_len = remaining_bytes;
+		}
 		cblk.len = c_len;
 		bzero(buf,CBUFSZ);
-		if((n = read(fd,buf,c_len)) != c_len){
-			fprintf(stderr,"read file fail!\n");
+		lseek(fd,c_off,SEEK_SET);
+		if((n = Read(fd,buf,c_len)) != c_len){
+			perror("client Read file");
+			fprintf(stderr,"%d bytes need but only %d bytes Read!\n",c_len,n);
 			goto over1;
 		}
+		*(buf + n) = '\0';
+		printf("content to calculate MD5 :\n%s\n",buf);
 		cal_md5(buf,c_len,cblk.md5);
-		if((n = write(connfd,&cblk,CHUNK_BLOCK_ENTRY_SZ)) != CHUNK_BLOCK_ENTRY_SZ){
-			fprintf(stderr,"write socket...\n");
+send_chunk_blk_entry:
+		if((n = Write(connfd,&cblk,CHUNK_BLOCK_ENTRY_SZ)) != CHUNK_BLOCK_ENTRY_SZ){
+			perror("client Write chunk_block_entry");
 			goto over1;
 		}
+		printf("chunk_block_entry		#%d sent successfully!\n",i);
+	}
+over2:
+	if(pthread_join(thid,NULL) != 0){
+		perror("pthread join");
 	}
 over1:
 	close(fd);
+	close(connfd);
     return 0;
 }
--- data_sync/s_sync.c
+++ data_sync/s_sync.c
@@ -1,9 +1,13 @@
 #include"glob.h"
 #include"rsync.h"
 static u8 buf[BUFSIZ];
-static u8 * ptr = buf + CHUNK_BLOCK_ENTRY_SZ;
+static u8 * ptr = buf + DELTA_BLOCK_ENTRY_SZ;
 int main()
 {
+	u64 blk_nr,blk_off;
+	u32 blk_len;
+	server_file_header sfh;
+	int i;
 	chunk_block_entry cblk;
 	u8 smd5[MD5_SZ+1];
 	delta_block_entry dblk;
@@ -12,7 +16,7 @@ int main()
 	int fd;
 	struct stat f_st;
 	u32 clnt_blksz;
-	u64 clnt_blknr;
+	u64 clnt_blknr,clnt_file_sz;
 	u8 file_name[FILE_NAME_LEN];
 	int port;
     int listenfd,connfd,len;
@@ -32,23 +36,27 @@ int main()
 	inet_ntop(AF_INET,(void*)&cli_addr.sin_addr,ip_str,INET_ADDRSTRLEN);
 	port = ntohs(cli_addr.sin_port);
 	printf(" from %s,port %d\n",ip_str,port);
-	/*-------------- read chunk header ---------*/
+	/*-------------- Read chunk header ---------*/
 	bzero(buf,BUFSIZ);
-	n = read(connfd,buf,CHUNK_FILE_HEAD_SZ);
+	n = Read(connfd,buf,CHUNK_FILE_HEAD_SZ);
 	if(n != CHUNK_FILE_HEAD_SZ){
-		fprintf(stderr,"read chunk_file_head fail!\n");
+		fprintf(stderr,"Read chunk_file_head fail!\n");
 		goto over2;
 	}
+	printf("---------------- chunk_file_header received! --------\n");
 	/* init from chunk header */
 	clnt_blksz = ((chunk_file_header *)buf)->block_sz;
-	clnt_blknr = ((chunk_file_header *)buf)->block_nr;
+	clnt_file_sz = ((chunk_file_header *)buf)->file_sz;
 	bzero(file_name,FILE_NAME_LEN);
 	strncpy(file_name,((chunk_file_header*)buf)->fn,FILE_NAME_LEN);
 	printf("clnt_blksz   -- %d\n",clnt_blksz);
-	printf("clnt_blknr   -- %ld\n",clnt_blknr);
+	printf("clnt_filesz  -- %d\n",clnt_file_sz);
 	printf("file to sync -- %s\n",file_name);
+	sfh.err = E_OK;
 	fd = open(file_name,O_RDONLY);
 	if(fd < 0){
+		if(errno == ENOENT)
+			sfh.err = E_FILE_NOT_EXIST;
 		perror("open");
 		goto over2;
 	}
@@ -58,101 +66,77 @@ int main()
 		goto over1;
 	}
 	serv_file_sz = f_st.st_size;
-	if(clnt_blknr == 0){
-		cblk.offset = 0;
-		goto overwrite;
-	}
-	while((n = read(connfd,&cblk,CHUNK_BLOCK_ENTRY_SZ)) == CHUNK_BLOCK_ENTRY_SZ){
-		out_of_loop_value = 0;
-		sz_to_send = DELTA_BLOCK_ENTRY_SZ;
-		dblk.offset = cblk.offset;
-		dblk.old_len = cblk.len;
-		dblk.embeded = BLK_SAME;
-		if(cblk.offset == serv_file_sz){
-			/*-------- case 1 --------*/
-			/* out of boundry,
-			 * truncate it */
-			printf("case 1 --------------- need truncate..........\n");
-			dblk.new_len = 0;
-			dblk.embeded = BLK_DIFF;
-			out_of_loop_value = 1;//for break
-		}else if(cblk.offset < serv_file_sz && cblk.len == clnt_blksz){
-			lseek(fd,cblk.offset,SEEK_SET);
-			remaining_bytes = serv_file_sz - cblk.offset;
-			if(remaining_bytes >= clnt_blksz){
-			/* the most common case */
-				/*-------- case 2 ---------*/
-				dblk.new_len = clnt_blksz;
-				n = read(fd,ptr,clnt_blksz);
-				cal_md5(ptr,clnt_blksz,smd5);
-				if(!MD5_EQUAL(cblk.md5,smd5)){
-					printf("case 2  ----------------------------------------md5 different!!!!\n");
-					dblk.embeded = BLK_DIFF;
-					sz_to_send += n;
-				}
-				printf("case 2  ----------------------------------------md5 same!!!!\n");
-				/* no append data */
-			}else if(remaining_bytes < clnt_blksz){
-				/*---------- case 3 ---------*/
-				/* remaining_bytes are less than clnt_blksz,
-				 * truncate */
-				printf("case 3 ------------------------ need truncate\n");
-				n = read(fd,ptr,remaining_bytes);
-				dblk.new_len = remaining_bytes;
-				dblk.embeded = BLK_DIFF;
-				sz_to_send += n;
-				out_of_loop_value = 3;
-			}
-		}else if(cblk.len < clnt_blksz){
-			/* last blk of client */
-			lseek(fd,cblk.offset,SEEK_SET);
-			remaining_bytes = serv_file_sz - cblk.offset;
-			if(remaining_bytes <= cblk.len){
-				/*--------- case 4 ---------*/
-				/* truncate */
-				if(remaining_bytes == cblk.len){
-					printf("case 4 ------------------ totally indentical file size,no need truncate \n");
-				}else{
-					printf("case 4 ------------------------ need truncate\n");
-				}
-				n = read(fd,ptr,remaining_bytes);
-				dblk.new_len = remaining_bytes;
-				dblk.embeded = BLK_DIFF;
-				sz_to_send += n;
-				out_of_loop_value = 4;
-			}else{
-				/*---------- case 5 --------*/
-				/* just overwrite from this blk */
-				printf("case 5 ------------------------ need overwrite \n");
-				out_of_loop_value = 5;
-				break;
-			}
-		}
-		memcpy(buf,&dblk,DELTA_BLOCK_ENTRY_SZ);
-//		if((n = write(connfd,buf,sz_to_send)) != sz_to_send){
-//			perror("write to socket");
-//			goto over1;
-//		}
-		if(out_of_loop_value != 0){break;}
-	}
-	if(out_of_loop_value != 5){
+	sfh.file_sz = serv_file_sz;
+	if((n = Write(connfd,&sfh,SERVER_FILE_HEADER_SZ)) != SERVER_FILE_HEADER_SZ){
+		perror("Write server_file_header");
 		goto over1;
 	}
-overwrite:
-	serv_off = cblk.offset;
-	printf("overwrite client replica from offset : %d\n",serv_off);
-	while(serv_off < serv_file_sz){
-		lseek(fd,serv_off,SEEK_SET);
-		dblk.offset = serv_off;
-		dblk.old_len = 0;
-		dblk.embeded = BLK_DIFF;
-		remaining_bytes = serv_file_sz - serv_off;
-		dblk.new_len = (remaining_bytes >= clnt_blksz?clnt_blksz:remaining_bytes);
-		sz_to_send = dblk.new_len + DELTA_BLOCK_ENTRY_SZ;
-		n = read(fd,ptr,dblk.new_len);
+	printf("server_file_header sent successfully!\n");
+	if(sfh.err != E_OK){
+		goto over1;
+	}
+	blk_nr = serv_file_sz/clnt_blksz;
+	if(serv_file_sz % clnt_blksz != 0){
+		blk_nr++;
+	}
+	printf("serv_file_sz		#%d\n",serv_file_sz);
+	printf("serv_block_nr		#%d\n",blk_nr);
+	for(i = 0;i < blk_nr;i++){
+		bzero(&cblk,CHUNK_BLOCK_ENTRY_SZ);
+		if((n = Read(connfd,&cblk,CHUNK_BLOCK_ENTRY_SZ)) != CHUNK_BLOCK_ENTRY_SZ){
+			perror("Read chunk blk entry");
+			goto over1;
+		}
+		printf("---------chunk_block_entry		#%d :\n",i);
+		printf("chunk_blk_offset	#%d\n",cblk.offset);
+		printf("chunk_blk_len		#%d\n",cblk.len);
+		sz_to_send = DELTA_BLOCK_ENTRY_SZ;
+		dblk.old_len = cblk.len;
+		dblk.offset = cblk.offset;
+		dblk.embeded = BLK_SAME;
+		blk_off = i*clnt_blksz;
+		blk_len = clnt_blksz;
+		if(blk_off != cblk.offset){
+			printf("blk_off == %d\n",blk_off);
+			fprintf(stderr,"blk_off != cblk.offset\n");
+			exit(1);
+		}
+		if(i == (blk_nr - 1)){
+			blk_len = serv_file_sz - blk_off;
+		}
+		dblk.new_len = blk_len;
+		if((n = Read(fd,ptr,blk_len)) != blk_len){
+			perror("Read blk");
+			goto over1;
+		}
+		*(ptr + n) = '\0';
+		printf("serv_blk_data --------\n");
+		printf("%s\n",ptr);
+		if(cblk.len == 0){
+			printf("chunk_block_entry len is 0,send the block and data to client\n");
+			printf("no need to calculate MD5\n");
+			dblk.embeded = BLK_DIFF;
+			sz_to_send += n;
+			printf("%d bytes data will be appened to delta_block!\n",n);
+			goto send_delta_blk;
+		}
+		cal_md5(ptr,clnt_blksz,smd5);
+		if(!MD5_EQUAL(cblk.md5,smd5)){
+			printf("--------- md5 different ----------\n");
+			dblk.embeded = BLK_DIFF;
+			sz_to_send += n;
+			printf("%d bytes data will be appened to delta_block!\n",n);
+		}else{
+			printf("--------- md5 same ----------\n");
+		}
+send_delta_blk:
 		memcpy(buf,&dblk,DELTA_BLOCK_ENTRY_SZ);
-		n = write(connfd,buf,sz_to_send);
-		serv_off += dblk.new_len;
+		if((n = Write(connfd,buf,sz_to_send)) != sz_to_send){
+			perror("Write socket");
+			goto over1;
+		}
+		printf("DELTA_BLOCK_ENTRY_SZ sz	%d\n",DELTA_BLOCK_ENTRY_SZ);
+		printf("%d bytes sent to client!\n",n);
 	}
 over1:
 	close(fd);
