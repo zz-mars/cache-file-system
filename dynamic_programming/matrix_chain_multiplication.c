317b0ea38977a9c6f71de7497dbab220e82bdca8
zz chen <grantchen88@gmail.com>
2013-09-30 16:14:43 +0800

    dp

--- dynamic_programming/matrix_chain_multiplication.c
+++ dynamic_programming/matrix_chain_multiplication.c
@@ -1,24 +1,31 @@
-#include"glob.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 //#define DBGMSG
 #define ZMAX_INT	0x7fffffff
 #define MATRIX_N	6
+
 #define MS_MATRIX_L	(MATRIX_N*MATRIX_N)
 
-static int ms_matrix[MS_MATRIX_L];
-static int p[MATRIX_N+1] = {30,35,15,5,10,20,25};
+static unsigned int ms_matrix[MS_MATRIX_L];
+static unsigned int p[MATRIX_N+1] = {30,35,15,5,10,20,25};
+
 #define INDEX_OF(i,j)	(((i)-1)*MATRIX_N+(j)-1)
 #define ZSET(i,j,v)		ms_matrix[INDEX_OF(i,j)] = v
 /*
 #define MSET(i,j,v)		ms_matrix[INDEX_OF(i,j)] = v
 #define SSET(i,j,v)		ms_matrix[INDEX_OF(j,i)] = v
 */
+
 #define M(i,j)			ms_matrix[INDEX_OF(i,j)]
 #define S(i,j)			ms_matrix[INDEX_OF(j,i)]
+
 static void init_ms_matrix(void)
 {
 	int i,j;
-	bzero(ms_matrix,sizeof(int)*MS_MATRIX_L);
+	memset(ms_matrix,0,sizeof(int)*MS_MATRIX_L);
 	for(i=1;i<=MATRIX_N;i++){
 		for(j=1;j<=MATRIX_N;j++){
 			if(i<j){
@@ -26,20 +33,20 @@ static void init_ms_matrix(void)
 			}
 		}
 	}
-	return;
 }
+
 static void prt_ms_matrix(void)
 {
 	int i,j;
 	for(i=1;i<=MATRIX_N;i++){
 		for(j=1;j<=MATRIX_N;j++){
-			printf("%3d ",ms_matrix[INDEX_OF(i,j)]);
+			printf("%3d ",M(i,j));
 		}
 		printf("\n");
 	}
 	printf("--------------------\n");
-	return;
 }
+
 static void prt_optimal_solution(int i,int j)
 {
 	int k;
@@ -52,36 +59,43 @@ static void prt_optimal_solution(int i,int j)
 	prt_optimal_solution(i,k);
 	prt_optimal_solution(k+1,j);
 	printf(")");
-	return;
 }
+
 static int recur_depth = 0;
+
 static int multi_chain_matrix_recursive_style(int i,int j)
 {
-	int k,v;
-	recur_depth++;
+	if(i > j) {
+		fprintf(stderr,"invalid arg!\n");
+		return -1;
+	}
+
+	if(M(i,j) < ZMAX_INT) {
+		goto ret;
+	}
+
 	if(i==j){
 		M(i,j) = 0;
-		return 0;
 	}else if((i+1) == j){
-		M(i,j) = p[i-1]*p[i]*p[j];
+		M(i,j) = p[i-1]*p[i]*p[i+1];
 		S(i,j) = i;
-		return M(i,j);
-	}
-	for(k=i;k<j;k++){
-		if(M(i,k) == ZMAX_INT){
-			multi_chain_matrix_recursive_style(i,k);
-		}
-		if(M(k+1,j) == ZMAX_INT){
-			multi_chain_matrix_recursive_style(k+1,j);
-		}
-		v = M(i,k) + M(k+1,j) + p[i-1]*p[k]*p[j];
-		if(v < M(i,j)){
-			M(i,j) = v;
-			S(i,j) = k;
+	}else {
+		int k;
+		recur_depth++;
+		for(k=i;k<j;k++){
+			unsigned int v = p[i-1]*p[k]*p[j] +	\
+							 multi_chain_matrix_recursive_style(i,k) +	\
+							 multi_chain_matrix_recursive_style(k+1,j);
+			if(v < M(i,j)){
+				M(i,j) = v;
+				S(i,j) = k;
+			}
 		}
 	}
+ret:
 	return M(i,j);
 }
+
 static void multi_chain_matrix(void)
 {
 	int i,j,k,v,l;
