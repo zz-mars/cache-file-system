c2126a30ffedf139392f12b9a1ef6b4f94ab387b
grantchen <grantchen88@gmail.com>
2015-04-23 23:53:08 +0800

    add find kth-largest

--- sort/qs.c
+++ sort/qs.c
@@ -34,10 +34,28 @@ void quick_sort(int a[], int s, int e) {
 	quick_sort(a, pivot+1, e);
 }
 
+int find_kth_largest(int a[], int n, int k) {
+	if(k >= n) {
+		return -1;
+	}
+	int pivot_idx = quick_partition(a, 0, n);
+	if(pivot_idx == -1) {
+		// fatal
+		return -1;
+	}
+	int reverse_idx = n - pivot_idx - 1;
+	if(reverse_idx == k) {
+		return a[pivot_idx];
+	}
+	return reverse_idx>k?find_kth_largest(a+pivot_idx+1, reverse_idx, k):find_kth_largest(a, pivot_idx, k-reverse_idx-1);
+}
+
 int main(int argc, char * argv[]) {
 	int a[ARRAY_LEN];
 	randGenerator(a, ARRAY_LEN, 500);
 	printIntarray(a, ARRAY_LEN);
+	int x = find_kth_largest(a, ARRAY_LEN, 3);
+	printf("third -> %d\n", x);
 	quick_sort(a, 0, ARRAY_LEN);
 //	quick_partition(a, 0, ARRAY_LEN);
 	printIntarray(a, ARRAY_LEN);
